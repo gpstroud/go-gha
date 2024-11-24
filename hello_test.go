f21cc6fd430119ffd659a35cf8f907b6602c906b
gpstroud <gpstroud@gmail.com>
2024-11-24 08:59:17 -0700

    Update hello_test.go

--- hello_test.go
+++ hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHelloWorld(t *testing.T) {
-    expected := "Hello World!"
-    actual := HelloWord()
+    expected := "Hello, World!"
+    actual := HelloWorld()
     if actual != expected {
         t.Errorf("Expected %s but got %s", expected, actual)
     }
